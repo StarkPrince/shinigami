6bcfb4b239fa45f56d185719d2e3abb3f3ba0fd9
Prince Raj <games.princeraj@gmail.com>
2021-05-17 01:50:29 +0530

    error and idky

--- src/BlogPosts/actions/index.js
+++ src/BlogPosts/actions/index.js
@@ -1,7 +1,7 @@
-import jsonplaceholder from '../apis/jsonPlaceHolder'
+import jsonPlaceHolder from '../apis/jsonPlaceHolder'
 
 export const fetchPosts = () => async dispatch =>
 {
-    const response = await jsonplaceholder.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    const response = await jsonPlaceHolder.get('/posts')
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
--- src/BlogPosts/components/PostList.js
+++ src/BlogPosts/components/PostList.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
-export class PostList extends Component
+class PostList extends Component
 {
     componentDidMount = () => this.props.fetchPosts();
+
+    renderList = () =>
+    {
+        return this.props.posts.map(post =>
+        {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
     render()
     {
+        console.log(this.props.posts)
         return (
-            <div >
-                <h1 style={{ fontFamily: 'Anurati' }}>Post List</h1>
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div >
         )
     }
 }
 
-const mapStateToProp = () =>
+const mapStateToProps = (state) =>
 {
-    return 'idk';
+    return { posts: state.posts }
 }
 
-export default connect(null, { fetchPosts })(PostList)
+export default connect(mapStateToProps, { fetchPosts })(PostList)
