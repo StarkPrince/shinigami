d7c9bdc4c7c53ba2644323730ce7e4a1db8d99a5
Prince Raj <games.princeraj@gmail.com>
2021-05-28 00:18:38 +0530

    added a memories project from javascript mastery deployed on netlify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './ExpenseTracker'
+import App from './memoriesProject/client'
 
 ReactDOM.render(<div><App /></div>,
     document.querySelector('#root')
